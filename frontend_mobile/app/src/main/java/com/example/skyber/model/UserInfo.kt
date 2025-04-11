6ea8a35e7b092e9cf3633ba198e31b8968d7332a
ethan max <ethanmaxine.enriquez@cit.edu>
2025-04-11 23:20:46 +0800

    User Login and Registration

---
+++ frontend_mobile/app/src/main/java/com/example/skyber/model/UserInfo.kt
@@ -0,0 +1,13 @@
+package com.example.skyber.model
+
+data class UserInfo(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    val birthdate: String,
+    val gender: String,
+    val age: Int,
+    val phoneNumber: String,
+    val address: String
+)
