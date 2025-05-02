9703129ae8bd5aa655d2d55e23ba3a70d083cb2b
ethan max <ethanmaxine.enriquez@cit.edu>
2025-05-02 17:11:24 +0800

    Added Files for increment 3

--- frontend_mobile/app/src/main/java/com/example/skyber/api/RetrofitClient.kt
+++ frontend_mobile/app/src/main/java/com/example/skyber/api/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // For Android Emulator localhost
+    private const val BASE_URL = "skyber.onrender.com" // For Android Emulator localhost
     // Use your computer's IP address if testing on physical device
     // private const val BASE_URL = "http://192.168.1.xxx:8080/"
 
